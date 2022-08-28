147b93098e49349f22e11d449a81edcb35b9e8b5
Bea Dobruski <bbeadobruski@gmail.com>
2022-08-28 20:32:26 -0300

    first commit :tada:

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="flex bg-black">
+      <form>
+        <input type="text" placeholder="Cidade" />
+        <button type="submit">Pesquisar</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
